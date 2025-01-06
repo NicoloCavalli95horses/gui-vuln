e745f791408bac4e59d7006461d4c5352c1d70a5
Nicolò Cavalli <94293531+NicoloCavalli95horses@users.noreply.github.com>
2025-01-06 14:12:36 +0100

    Create Loading.vue

---
+++ src/components/Loading.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="bg">
+    <p>Loading data...</p>
+  </div>
+</template>
+
+<script setup>
+//====================================
+// Consts
+//====================================
+</script>
+
+<style lang="scss" scoped>
+.bg {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.7);
+  display: grid;
+  place-content: center;
+}
+</style>
